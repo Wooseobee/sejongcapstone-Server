9cec005fa3284716f8ce7a3684c570de6aae6c5f
wooseobee <dntjq0807@naver.com>
2023-05-10 18:18:08 +0900

    refactor: 회원가입 요청 검증로직 수정

--- src/main/java/capstone3d/Server/exception/GlobalExceptionHandler.java
+++ src/main/java/capstone3d/Server/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package capstone3d.Server.exception;
 
 import capstone3d.Server.response.AllResponse;
+import capstone3d.Server.response.StatusMessage;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +14,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<AllResponse> handle(BadRequestException e) {
         return AllResponse.ErrorResponseEntity(e.getStatusMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<AllResponse> handleValidError(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+
+        return AllResponse.ValidErrorResponseEntity(StatusMessage.SignUp_Request_Error.getStatus(),bindingResult);
     }
 }
